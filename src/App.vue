c1a1e783b614379917f71ec28f45efe54df28e8e
kabouras <opiliokabouras@gmail.com>
2021-02-09 00:48:53 -0500

    splash

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
+    <div class="shround animate__animated animate__fadeOut animate__slower">
+      <div class="modal">
+        <h1>Terminal Hacking Game</h1>
+      </div>
+    </div>
   <div id="app">
-    <div class="container">
-      <h1>Terminal Hacking Game</h1>
+    <div class="container">    
       <div class="terminal-outer-1">
         <GameBoard />
       </div>
@@ -19,4 +23,25 @@ export default {
   }  
 }
 </script>
+
+<style>
+.shround {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background-color: rgba(61, 54, 53, .5);
+  z-index: 100;
+  pointer-events: none;
+}
+.modal {
+  max-width: 700px;  
+  margin: 0 auto;
+  margin-top: 5em;
+  font-size: 2.5em;
+  pointer-events: none;
+}
+</style>
+
 
